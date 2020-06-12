b58766ca2b26166a8bc0a36b973e50287f16cf8c
zach ryan <zachryan@pop-os.localdomain>
2020-06-12 15:35:58 -0600

    web scraper V=2.0, have added the ability to obtain all bible verses from any language/edition. Still need to work on titles.

--- bible.com_web_scraper.py
+++ bible.com_web_scraper.py
@@ -1,17 +1,16 @@
 import requests
 import urllib.request
+import os
 import time
 from bs4 import BeautifulSoup
 import re
 from nltk.tokenize import word_tokenize
+#------------IMPORTANT------------#
+#2524 total bibles on bible.com
 
-def main():
-    #this is a long string that is printed when asked by the user
-    book_table_of_contents = "Book 01        Genesis\n Book 02        Exodus\n Book 03        Leviticus\n Book 04        Numbers\n Book 05        Deuteronomy\n Book 07        Judges\n Book 06        Joshua\n Book 08        Ruth\n Book 09        1 Samuel\n Book 10        2 Samuel\n Book 11        1 Kings\n Book 12        2 Kings\n Book 13        1 Chronicles\n Book 14        2 Chronicles\n Book 15        Ezra\n Book 16        Nehemiah\n Book 17        Esther\n Book 18        Job\n Book 19        Psalms\n Book 20        Proverbs\n Book 21        Ecclesiastes\n Book 22        Song of Solomon\n Book 23        Isaiah\n Book 24        Jeremiah\n Book 25        Lamentations\n Book 26        Ezekiel\n Book 27        Daniel\n Book 28        Hosea\n Book 29        Joel\n Book 30        Amos\n Book 31        Obadiah\n Book 32        Jonah\n Book 33        Micah\n Book 34        Nahum\n Book 35        Habakkuk\n Book 36        Zephaniah\n Book 37        Haggai\n Book 38        Zechariah\n Book 39        Malachi\n Book 40        Matthew\n Book 41        Mark\n Book 42        Luke\n Book 43        John\n Book 44        Acts\n Book 45        Romans\n Book 46        1 Corinthians\n Book 47        2 Corinthians\n Book 48        Galatians\n Book 49        Ephesians\n Book 50        Philippians\n Book 51        Colossians\n Book 52        1 Thessalonians\n Book 53        2 Thessalonians\n Book 54        1 Timothy\n Book 55        2 Timothy\n Book 56        Titus\n Book 57        Philemon\n Book 58        Hebrews\n Book 59        James\n Book 60        1 Peter\n Book 61        2 Peter\n Book 62        1 John\n Book 63        2 John\n Book 64        3 John\n Book 65        Jude\n Book 66        Revelation\n"
 
-    #url = 'https://www.bible.com/bible/116/MRK.1.NLT'
-    url = input("Please enter the url you want to scrape from bible.com: \n")
-    #print(url)
+def page_scraper(url, book, book_number, chapter_number, lang_code):
+
     response = requests.get(url)
 
     #grap the file name to be created from the url
@@ -20,24 +19,13 @@ def main():
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-
-    #get the book and chapter number from the user to print inside the final document
-    book_number = -100
-    chapter_number = -100
-
-    while book_number < 0:
-        book_number = input("Please enter the books number or type '-999' for a list of books and corresponding number: ")
-        book_number = int(book_number)
-        if book_number == -999:
-            print(book_table_of_contents)
-
-    while chapter_number < 0:
-        chapter_number = input("Please enter the chapter number(001, 002, 003, ..., 010, 011, 012, ...), if this section of the Bible does not have chapters press 1: ")
-        chapter_number = int(chapter_number)
-
-
     file_name = str(file_name.group())[1:]
-    file = open(str(file_name)+".txt", "w")
+    if not os.path.exists("/" + str(lang_code) + "/" + str(book)):
+        os.makedirs(str(lang_code)+ "/" + str(book_number) + "_" + str(book), exist_ok = True)
+        file = open(str(lang_code) + "/" + str(book_number) + "_" + str(book) + "/" + str(file_name)+".txt", "w")
+    else:
+        file = open(str(lang_code) + "/" + str(book_number) + "_" + str(book) + "/" + str(file_name)+".txt", "w")
+
 
     #get all the tags that contain a verse
     all_tags = soup.findAll('span', class_=re.compile("(verse v[0-9]+)"))
@@ -59,9 +47,8 @@ def main():
                 #tokenize all words and symbols before writing to file
                 token = word_tokenize(current_working_verse.replace("’", "'"))
                 tokened_working_verse = ' '.join(token)
-                print(current_working_verse)
-                print(token)
-                print(tokened_working_verse)
+                #print(current_working_verse)
+                #print(token)
                 #print(tokened_working_verse)
                 if len(str(verse_count)) == 1:
                     file.write(str(book_number) + ":" + str(chapter_number) + ":00" + str(verse_count) + ":0\t" + tokened_working_verse + "\n")
@@ -116,4 +103,51 @@ def main():
     file.close()
     return 0
 
-main()
+def driver():
+    book_table_of_contents_dic = {"GEN" : 1, "EXO" : 2, "LEV" : 3, "NUM" : 4, "DEU" : 5, "JOS" : 6, "JDG" : 7, "RUT" : 8, "1SA" : 9, "2SA" : 10, "1KI" : 11, "2KI" : 12, "1CG" : 13, "2CH" : 14, "EZR" : 15, "NEH" : 16, "EST" : 17, "JOB" : 18, "PSA" : 19, "PRO" : 20, "ECC" : 21, "SNG" : 22, "ISA" : 23, "JER" : 24, "LAM" : 25, "EZK" : 26, "DAN" : 27, "HOS" : 28, "JOL" : 29, "AMO" : 30, "OBA" : 31, "JON" : 32, "MIC" : 33, "NAH" : 34, "HAB" : 35, "ZEP" : 36, "HAG" : 37, "ZEC" : 38, "MAL" : 39, "MAT" : 40, "MRK" : 41, "LUK" : 42, "JHN" : 43, "ACT" : 44, "ROM" : 45, "1CO" : 46, "2CO" : 47, "GAL" : 48, "EPH" : 49, "PHP" : 50, "COL" : 51, "1TH" : 52, "2TH" : 53, "1TI" : 54, "2TI" : 55, "TIT" : 56, "PHM" : 57, "HEB" : 58, "JAS" : 59, "1PE" : 60, "2PE" : 61, "1JN" : 62, "2JN" : 63, "3JN" : 64, "JUD" : 65, "REV" : 66}
+    #loop through all 2524 bibles
+    #change the numbers to scrape specific bibles,
+    #lets say you only want bible 1245, then it would be range(1245, 1246)
+    #all ->  range(1, 2525)
+    #first -> range(1,2)
+    for i in range(1,2):
+        #get the language code
+        lang_code = get_lang_code(i)
+        #if lang code comes back false, then we know its a bad url
+        if lang_code != False:
+            #if the lang code is good then we want to loop through all the books in the bible
+            for book in book_table_of_contents_dic:
+                chapter_count = 1
+                status = True
+                #for each book we also need to loop though all the chapters
+                #status changes to false when a book or chapter is not in this specific edition of the bible.
+                #due to the way bible.com works, if you throw a bad url at it but the 'bible.com/bible/' is there it automaticaly brings you to 'bible.com/bible/1/GEN.1.KJV'
+                #comparing the url given back to the one created, if they match then we are good to scrape that page
+                #if they do not match then we know to move on to the next book/chapter
+                while status == True:
+                    url = "https://www.bible.com/bible/" + str(i) + "/" + str(book) + "." + str(chapter_count) + "." + str(lang_code)
+                    response =  requests.get(url)
+                    print(response.url)
+                    if response.url != url:
+                        status = False
+                    else:
+                        print(url)
+                        #after satisfying all othre conditions we then scrape the web page and store the data
+                        page_scraper(url, book, book_table_of_contents_dic[book], chapter_count, lang_code)
+                        chapter_count += 1
+        else:
+            continue
+
+def get_lang_code(i):
+    #this grabs the language code for the current bible being checked.
+    url = "https://www.bible.com/bible/" + str(i)
+    response = requests.get(url)
+    if response.status_code == 200:
+        lang_code = re.search(".[a-zA-Z0-9]*$", response.url)
+        return lang_code.group()[1:]
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    driver()
---
+++ test.py
@@ -0,0 +1,30 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import re
+import os
+
+url = "https://www.bible.com/bible/3453/GEN.5565.KsddJV"
+
+response = requests.get(url)
+
+lang_code = re.search(".[a-zA-Z0-9]*$", response.url)
+
+print(response.status_code)
+print(response.url)
+print(lang_code.group()[1:])
+print(response)
+#print(response.text)
+book_table_of_contents_dic = {"GEN" : 1, "EXO" : 2, "LEV" : 3, "NUM" : 4, "DEU" : 5, "JOS" : 6, "JDG" : 7, "RUT" : 8, "1SA" : 9, "2SA" : 10, "1KI" : 11, "2KI" : 12, "1CG" : 13, "2CH" : 14, "EZR" : 15, "NEH" : 16, "EST" : 17, "JOB" : 18, "PSA" : 19, "PRO" : 20, "ECC" : 21, "SNG" : 22, "ISA" : 23, "JER" : 24, "LAM" : 25, "EZK" : 26, "DAN" : 27, "HOS" : 28, "JOL" : 29, "AMO" : 30, "OBA" : 31, "JON" : 32, "MIC" : 33, "NAH" : 34, "HAB" : 35, "ZEP" : 36, "HAG" : 37, "ZEC" : 38, "MAL" : 39, "MAT" : 40, "MRK" : 41, "LUK" : 42, "JHN" : 43, "ACT" : 44, "ROM" : 45, "1CO" : 46, "2CO" : 47, "GAL" : 48, "EPH" : 49, "PHP" : 50, "COL" : 51, "1TH" : 52, "2TH" : 53, "1TI" : 54, "2TI" : 55, "TIT" : 56, "PHM" : 57, "HEB" : 58, "JAS" : 59, "1PE" : 60, "2PE" : 61, "1JN" : 62, "2JN" : 63, "3JN" : 64, "JUD" : 65, "REV" : 66}
+
+#for item in book_table_of_contents_dic:
+#    print(book_table_of_contents_dic[item])
+
+
+if not os.path.exists("/hello"):
+    os.mkdir("/hello")
+    if not os.path.exists("/hello/world"):
+        os.mkdir()
+
+soup = BeautifulSoup(response.text, "html.parser")
