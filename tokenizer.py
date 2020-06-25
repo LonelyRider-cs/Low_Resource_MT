7be47239e2601322ccdbf1cd43889e874f77ae31
lingliu <ling.liu@colorado.edu>
2020-06-24 23:02:42 -0600

    add tokenization option to merge.py

---
+++ tokenizer.py
@@ -0,0 +1,19 @@
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+
+def tokenize(text_string, tokenizer_type):
+    '''
+    Given a string of text and a type of tokenizer to use;
+    Return the tokenized string of text.
+    '''
+    if tokenizer_type is None:
+        tokened = text_string
+    elif tokenizer_type.lower() == 'eng' or tokenizer_type.lower()[:3] == 'eng':
+        tokens = word_tokenize(text_string.replace("’", "'").replace("—", " — "))
+        tokened = ' '.join(tokens)
+    elif tokenizer_type.lower() == 'other':
+        tokenizer = RegexpTokenizer("[^\s.\";:,.“”\[\(\)?!]+|[^\w\d'\s\-]")
+        tokens = tokenizer.tokenize(text_string)
+        tokened = ' '.join(tokens)
+
+    return tokened
