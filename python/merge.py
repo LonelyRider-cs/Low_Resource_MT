5b33924de7364fa5531f766e548db0c54cf1a8e6
lingliu <ling.liu@colorado.edu>
2020-07-11 17:21:58 -0600

    update tokenizer

--- python/merge.py
+++ python/merge.py
@@ -1,5 +1,5 @@
 import os, random, argparse
-from tokenizer import tokenize
+from tokenizer import preprocess, sentencepiece_train, sentencepiece_segment
 
 def read_data(dirname):
     id2text = {}
@@ -14,7 +14,7 @@ def read_data(dirname):
                         count += 1
                         lines = line.rstrip('\n').split('\t')
                         if len(lines) == 2:
-                            id2text[lines[0]] = lines[1]
+                            id2text[lines[0].strip()] = lines[1].strip()
                         else:
                             print("--------Please make sure Line {} in {} is in right format--------".format(count, fname))
                             format_problem = True
@@ -23,6 +23,20 @@ def read_data(dirname):
                 break
         if format_problem:
             break
+    return id2text
+
+def read_tokenized_data(fname):
+    id2text = {}
+    with open(fname) as f:
+        count = 0
+        for line in f:
+            count += 1
+            lines = line.rstrip('\n').split('\t')
+            if len(lines) == 2:
+                id2text[lines[0].strip()] = lines[1].strip()
+            else:
+                print("--------Please make sure Line {} in {} is in right format--------".format(count, fname))
+                break
     return id2text
 
 def checkOverlap(srclang_id2text, tgtlang_id2text2, verbose=False):
@@ -44,35 +58,118 @@ def checkOverlap(srclang_id2text, tgtlang_id2text2, verbose=False):
     if verbose:
         print('Source language lines missing in target language: \n{}\n'.format(srconly))
         print('Target language lines missing in source language: \n{}'.format(tgtonly))
-    return list(src_ids.intersection(tgt_ids))
+    common_lines = []
+    for item in srclang_id2text.keys():
+        if item in tgt_ids:
+            common_lines.append(item)
+    return common_lines
+    # return list(src_ids.intersection(tgt_ids))
 
+
+def _tokenize_preprocess(id2text, idlist, language, tokenizer, syllabifier, ftmp, fall, fmap):
+    tokenized_id2text = {id: preprocess(text, language, tokenizer, syllabifier)
+                         for id, text in id2text.items()}
+    for id, text in tokenized_id2text.items():
+        ftmp.write(text + '\n')
+        fall.write(str(id) + '\t' + text + '\n')
+    for id in idlist:
+        text = tokenized_id2text[id]
+        fmap.write(text + '\n')
+
+def _tokenize_sentencepiece(id2text, training_file, model_dir, language, vocab_size, user_defined_symbols=[]):
+    # train sentencepiece model
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    model_prefix = os.path.join(model_dir, language)
+    sentencepiece_train(training_file, model_prefix, vocab_size, user_defined_symbols)
+
+    # segment text with sentencepiece model
+    id2sentencepiece_text = {}
+    model = model_prefix + '.model'
+    count = 0
+    for id, text in id2text.items():
+        if not count%1000:
+            print('> line {}'.format(id))
+            print('> before sentencepiece segmentation:', text)
+        text = sentencepiece_segment(text, model)
+        id2sentencepiece_text = text
+        if not count % 1000:
+            print('> after sentencepiece segmentation:', text)
+            print()
+        count += 1
+    return id2sentencepiece_text
+
+def _write_to_file(id2text, idlist, fall, fmapped):
+    for id, text in id2text.items():
+        fall.write(str(id) + '\t' + text + '\n')
+    for id in idlist:
+        text = id2text[id]
+        fmapped.write(text + '\n')
 
 def map_data(srclang_id2text, tgtlang_id2text, idlist,
              srclang, tgtlang,
              source_tokenizer = None, target_tokenizer = None,
              source_syllabifier = None, target_syllabifier = None,
-             datatype = None):
+             source_sentencepiece = None, target_sentencepiece = None,
+             datatype = 'shared'):
     srclang_name = srclang.split('/')[-1]
     tgtlang_name = tgtlang.split('/')[-1]
     direction = srclang_name + '-' + tgtlang_name
-    if datatype:
-        inputfile = datatype + '.' + direction + '.input'
-        outputfile = datatype + '.' + direction + '.output'
-    else:
-        inputfile = direction + '.input'
-        outputfile = direction + '.output'
+    inputfile = datatype + '.' + direction + '.input'
+    outputfile = datatype + '.' + direction + '.output'
+    all_src = srclang_name + '.txt'
+    all_tgt = tgtlang_name + '.txt'
+    tmp_src = srclang_name + '.tmp'
+    tmp_tgt = tgtlang_name + '.tmp'
 
-    with open(inputfile, 'w') as fin, open(outputfile, 'w') as fout:
-        for id in idlist:
-            fin.write(tokenize(srclang_id2text[id], srclang_name, source_tokenizer, source_syllabifier))
-            fin.write('\n')
-            fout.write(tokenize(tgtlang_id2text[id], tgtlang_name, target_tokenizer, target_syllabifier))
-            fout.write('\n')
+    output_dir = "../data_in_MT_format/"+direction
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-def splitBYlines(srclang_id2text, tgtlang_id2text, idlist,
-                 srclang, tgtlang,
-                 source_tokenizer = None, target_tokenizer = None,
-                 source_syllabifier = None, target_syllabifier = None):
+    inputfile = os.path.join(output_dir, inputfile)
+    outputfile = os.path.join(output_dir, outputfile)
+    all_src = os.path.join(output_dir, all_src)
+    all_tgt = os.path.join(output_dir, all_tgt)
+    tmp_src = os.path.join(output_dir, tmp_src)
+    tmp_tgt = os.path.join(output_dir, tmp_tgt)
+
+    with open(tmp_src, 'w') as ftmp_src, open(tmp_tgt, 'w') as ftmp_tgt, \
+        open(all_src, 'w') as fall_src, open(all_tgt, 'w') as fall_tgt, \
+        open(inputfile, 'w') as fin, open(outputfile, 'w') as fout:
+        _tokenize_preprocess(srclang_id2text, idlist, srclang,
+                    source_tokenizer,
+                    source_syllabifier,
+                    ftmp_src, fall_src, fin)
+        _tokenize_preprocess(tgtlang_id2text, idlist, tgtlang,
+                    target_tokenizer,
+                    target_syllabifier,
+                    ftmp_tgt, fall_tgt, fout)
+
+    model_dir = '../sentencepiece_models/'
+    if source_sentencepiece:
+        print('>>> source sentencepiece:', source_sentencepiece)
+        srclang_id2text = _tokenize_sentencepiece(id2text=read_tokenized_data(all_src),
+                                        training_file=tmp_src,
+                                        model_dir=model_dir,
+                                        language=srclang_name,
+                                        vocab_size=source_sentencepiece,
+                                        user_defined_symbols=[])
+    if target_sentencepiece:
+        print('>>> target sentencepiece:', target_sentencepiece)
+        tgtlang_id2text = _tokenize_sentencepiece(id2text=read_tokenized_data(all_tgt),
+                                        training_file=tmp_tgt,
+                                        model_dir=model_dir,
+                                        language=tgtlang_name,
+                                        vocab_size=target_sentencepiece,
+                                        user_defined_symbols=[])
+
+    with open(all_src, 'w') as fall_src, open(all_tgt, 'w') as fall_tgt, \
+        open(inputfile, 'w') as fin, open(outputfile, 'w') as fout:
+        _write_to_file(srclang_id2text, idlist, fall_src, fin)
+        _write_to_file(tgtlang_id2text, idlist, fall_tgt, fout)
+    os.system('rm '+output_dir+'/*.tmp')
+
+def splitBYlines(idlist, srclang, tgtlang):
     """
     split the lines into train-validation-test by the ratio: 7:1:2
     """
@@ -82,38 +179,61 @@ def splitBYlines(srclang_id2text, tgtlang_id2text, idlist,
     train_idlist = idlist[:train_boundary]
     dev_idlist = idlist[train_boundary:dev_boundary]
     test_idlist = idlist[dev_boundary:]
-    map_data(srclang_id2text, tgtlang_id2text, train_idlist,
-             srclang=srclang, tgtlang=tgtlang,
-             source_tokenizer =  source_tokenizer, target_tokenizer = target_tokenizer,
-             source_syllabifier = source_syllabifier, target_syllabifier=target_syllabifier,
-             datatype='train')
-    map_data(srclang_id2text, tgtlang_id2text, dev_idlist,
-             srclang=srclang, tgtlang=tgtlang,
-             source_tokenizer = source_tokenizer, target_tokenizer = target_tokenizer,
-             source_syllabifier = source_syllabifier, target_syllabifier=target_syllabifier,
-             datatype='dev')
-    map_data(srclang_id2text, tgtlang_id2text, test_idlist,
-             srclang=srclang, tgtlang=tgtlang,
-             source_tokenizer = source_tokenizer, target_tokenizer = target_tokenizer,
-             source_syllabifier = source_syllabifier, target_syllabifier=target_syllabifier,
-             datatype='test')
+
+    srclang_name = srclang.split('/')[-1]
+    tgtlang_name = tgtlang.split('/')[-1]
+    direction = srclang_name + '-' + tgtlang_name
+    datadir = "../data_in_MT_format/" + direction
+    srcdata = os.path.join(datadir, srclang_name + '.txt')
+    tgtdata = os.path.join(datadir, tgtlang_name + '.txt')
+    srclang_id2text = read_tokenized_data(srcdata)
+    tgtlang_id2text = read_tokenized_data(tgtdata)
+
+    for datatype, idlist in zip(['train', 'dev', 'test'], [train_idlist, dev_idlist, test_idlist]):
+        inputfile = os.path.join(datadir, datatype + '.' + direction + '.input')
+        outputfile = os.path.join(datadir, datatype + '.' + direction + '.output')
+        with open(inputfile, 'w') as fin, open(outputfile, 'w') as fout:
+            for id in idlist:
+                fin.write(srclang_id2text[id] + '\n')
+                fout.write(tgtlang_id2text[id] + '\n')
 
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--source", type = str, required=True, help = "dir name for source language")
-    parser.add_argument("-t", "--target", type=str, required=True, help="dir name for target language")
-    parser.add_argument("-split", "--split", action='store_true', required=False, help="If specified, split the data lines into train-dev-test with the ratio of 7:1:2")
-    parser.add_argument("-stk", "--source_tokenizer", type = str, required=False,
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-s", "--source",
+                        type = str, required=True,
+                        help = "dir name for source language")
+    parser.add_argument("-t", "--target",
+                        type=str, required=True,
+                        help="dir name for target language")
+    parser.add_argument("-split", "--split",
+                        action='store_true', required=False,
+                        help="If specified, split the data lines into train-dev-test with the ratio of 7:1:2")
+    parser.add_argument("-stk", "--source_tokenizer",
+                        type = str, required=False,
                         help="For the source language, you can specify '-stk eng' to tokenize the text with the English tokenizer, or '-stk other' to tokenize the text with the tokenizer for other languages. When not specified or if you specify tokenizer names other than 'eng' and 'other', no tokenization is conducted")
-    parser.add_argument("-ttk", "--target_tokenizer", type=str, required=False,
+    parser.add_argument("-ttk", "--target_tokenizer",
+                        type=str, required=False,
                         help="For the target language, you can specify '-stk eng' to tokenize the text with the English tokenizer, or '-stk other' to tokenize the text with the tokenizer for other languages. When not specified or if you specify tokenizer names other than 'eng' and 'other', no tokenization is conducted")
-    parser.add_argument("-ssl", "--source_syllabifier", type=str, required=False,
-                        help="For the source language, you can specify '-ssl eus' to break words in to syllables by the Basque syllabifier. Currently, only Basque syllabifier is provided. More syllabifier choices will be added. Please install foma first if you want to use the syllabifier.")
-    parser.add_argument("-tsl", "--target_syllabifier", type=str, required=False,
-                        help="For the target language, you can specify '-tsl eus' to break words in to syllables by the Basque syllabifier. Currently, only Basque syllabifier is provided. More syllabifier choices will be added. Please install foma first if you want to use the syllabifier.")
-    parser.add_argument("-v", "--verbose", action='store_true', required=False,
+    parser.add_argument("-ssl", "--source_syllabifier",
+                        type=str, required=False,
+                        help="For the source language, you can specify '-ssl eus' to break words in to syllables by the Basque syllabifier. Currently, Basque (eus) and Navajo (nav) syllabifiers are provided. More syllabifier choices will be added.")
+    parser.add_argument("-tsl", "--target_syllabifier",
+                        type=str, required=False,
+                        help="For the target language, you can specify '-tsl eus' to break words in to syllables by the Basque syllabifier. Currently, Basque (eus) and Navajo (nav) syllabifiers are provided. More syllabifier choices will be added.")
+    parser.add_argument("-ssp", "--source_sentencepiece",
+                        type=int, required=False,
+                        help="When specified, a sentencepiece model will be trained  and applied to segmented the source language text. You can specify the vocabulary size by specifying an integer. Suggest values are 8000, 16000, 32000, and the number should be smaller than the vocabulary size before sentencepiece.")
+    parser.add_argument("-tsp", "--target_sentencepiece",
+                        type=int, required=False,
+                        help="When specified, a sentencepiece model will be trained and applied to segmented the target language text. You can specify the vocabulary size by specifying an integer. Suggest values are 8000, 16000, 32000, and the number should be smaller than the vocabulary size before sentencepiece.")
+    parser.add_argument("-v", "--verbose",
+                        action='store_true', required=False,
                         help="If specified, print out the lines only the source language or only the target language")
+    parser.add_argument("-h", "--help",
+                        action="help", default=argparse.SUPPRESS,
+                        help="convert the source-target languages into the format needed for MT with Fairseq. The converted data will be stored in '../data_in_MT_format/SOURCE-TARGET/'")
+
 
     args = parser.parse_args()
 
@@ -141,16 +261,24 @@ def main():
     else:
         target_syllabifier = None
 
-    if args.split:
-        splitBYlines(srclang_id2text, tgtlang_id2text, common_lines,
-                     srclang = srclang, tgtlang = tgtlang,
-                     source_tokenizer = source_tokenizer, target_tokenizer = target_tokenizer,
-                     source_syllabifier = source_syllabifier, target_syllabifier=target_syllabifier)
+    if args.source_sentencepiece:
+        source_sentencepiece = args.source_sentencepiece
     else:
-        map_data(srclang_id2text, tgtlang_id2text, common_lines,
-                 srclang=srclang, tgtlang=tgtlang,
-                 source_tokenizer = source_tokenizer, target_tokenizer = target_tokenizer,
-                 source_syllabifier = source_syllabifier, target_syllabifier=target_syllabifier)
+        source_sentencepiece = None
+    if args.target_sentencepiece:
+        target_sentencepiece = args.target_sentencepiece
+    else:
+        target_sentencepiece = None
+
+    map_data(srclang_id2text, tgtlang_id2text, common_lines,
+             srclang=srclang, tgtlang=tgtlang,
+             source_tokenizer=source_tokenizer, target_tokenizer=target_tokenizer,
+             source_syllabifier=source_syllabifier, target_syllabifier=target_syllabifier,
+             source_sentencepiece=source_sentencepiece, target_sentencepiece=target_sentencepiece)
+
+    if args.split:
+        splitBYlines(common_lines, srclang, tgtlang)
+
 
 
 
--- python/tokenizer.py
+++ python/tokenizer.py
@@ -5,51 +5,105 @@ Please make sure your have foma.py in the same directory as this file.
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import RegexpTokenizer
 import foma
+import sentencepiece as spm
 
 
-def tokenize(text_string, language = None, tokenizer_type = None, syllabifier_type = None):
+def normalize_navajo(text: str) -> str:
+    """
+    Some Navajo sources which use RIGHT SINGLE QUOTATION MARK (2019) ’ or MODIFIER LETTER APOSTROPHE (02BC) ʼ for apostrophe.
+    This is to convert these symbols to apostrohes.
+    """
+    return text.replace("’", "'").replace("ʼ", "'")
+
+def basic_tokenizer(text: str, tokenizer_type = None) -> str:
+    """
+    segment punctuations from words
+    """
+    if tokenizer_type is None:
+        segmented = text
+    elif tokenizer_type.lower() == 'eng' or tokenizer_type.lower()[:3] == 'eng':
+        tokens = word_tokenize(text.replace("’", "'").replace("—", " — "))
+        segmented = ' '.join(tokens)
+    elif tokenizer_type.lower() == 'other':
+        tokenizer = RegexpTokenizer("[^\s.\";:,.“”\[\(\)?!]+|[^\w\d'\s\-]")
+        tokens = tokenizer.tokenize(text)
+        segmented = ' '.join(tokens)
+    else:
+        print("!!! Unrecognized specifier name! Please make sure you specified a valid tokenizer.")
+    return segmented
+
+def syllabifier(text: str, syllabifier_type = None) -> str:
+    """
+    segment words by using foma transducer syllabifiers
+    """
+    if 'eus' in syllabifier_type:
+        fomafile = "foma_tokenizer/eus_wordtokenizer.fomabin"
+    elif 'nav' in syllabifier_type:
+        fomafile = "foma_tokenizer/nav_wordtokenizer.fomabin"
+    else:
+        print("!!! Unrecognized syllabifier name! Please make sure you have a .fomabin file for the language you specify")
+    syllabifier = foma.FST.load(fomafile)
+    wordlist_syllabified = []
+    for token in text.split():
+        syllabified = syllabifier[token][0].decode("utf-8")
+        # syllabified = syllabified.replace('@', '')
+        syllabified = syllabified.replace('@ @', '@@ ')
+        wordlist_syllabified.append(syllabified)
+    outputstring = ' '.join(wordlist_syllabified)
+    return outputstring
+
+
+def preprocess(text, language=None, tokenizer_type=None, syllabifier_type = None):
     '''
     Given a string of text, a type of tokenizer to use, and a type of syllabifier to apply;
     Return the text in the format of the tokenized string each breaks into a sequence of syllables.
     '''
+    # normalized navajo data
     if language.lower() in ['navajo', 'nvjob']:
-        # Some Navajo sources which use
-        # RIGHT SINGLE QUOTATION MARK (2019) ’
-        # or MODIFIER LETTER APOSTROPHE (02BC) ʼ
-        # for apostrophe.
-        # This is to convert these symbols to apostrohes.
-        text_string = text_string.replace("’", "'").replace("ʼ", "'")
+        normalize_navajo(text)
 
-    if tokenizer_type is None:
-        # tokened = text_string
-        tokens = text_string.split()
-    elif tokenizer_type.lower() == 'eng' or tokenizer_type.lower()[:3] == 'eng':
-        tokens = word_tokenize(text_string.replace("’", "'").replace("—", " — "))
-        # tokened = ' '.join(tokens)
-    elif tokenizer_type.lower() == 'other':
-        tokenizer = RegexpTokenizer("[^\s.\";:,.“”\[\(\)?!]+|[^\w\d'\s\-]")
-        tokens = tokenizer.tokenize(text_string)
-        # tokened = ' '.join(tokens)
-    else:
-        # tokened = text_string
-        tokens = text_string.split()
+    # segment punctuations from words
+    segmented = basic_tokenizer(text, tokenizer_type)
 
+    # break words into syllables
     if syllabifier_type:
-        if 'eus' in syllabifier_type:
-            fomafile = "foma_tokenizer/eus_wordtokenizer.fomabin"
-        elif 'nav' in syllabifier_type:
-            fomafile = "foma_tokenizer/nav_wordtokenizer.fomabin"
-        else:
-            print("!!!please make sure you have a .fomabin file for the language you specify")
-        syllabifier = foma.FST.load(fomafile)
-        wordlist_syllabified = []
-        for token in tokens:
-            syllabified = syllabifier[token][0].decode("utf-8")
-            # syllabified = syllabified.replace('@', '')
-            syllabified = syllabified.replace('@ @', '@@ ')
-            wordlist_syllabified.append(syllabified)
-        outputstring = ' '.join(wordlist_syllabified)
-    else:
-        outputstring = ' '.join(tokens)
+        segmented = syllabifier(segmented, syllabifier_type)
 
-    return outputstring
+    return segmented
+
+
+def sentencepiece_train(training_file, model_prefix, vocab_size, user_defined_symbols=[]):
+    """
+    train a BPE model
+    """
+    spm.SentencePieceTrainer.train(input=training_file, model_prefix=model_prefix, vocab_size=vocab_size, user_defined_symbols=user_defined_symbols)
+
+def sentencepiece_segment(text: str, model_file) -> str:
+    # segmentation
+    sp = spm.SentencePieceProcessor(model_file=model_file)
+    text_bpe = sp.encode(text, out_type=str)
+    text_bpe = " ".join(text_bpe)
+    return text_bpe
+
+
+
+
+# def tokenize(text, language = None, tokenizer_type = None, syllabifier_type = None, bpe = None):
+#     '''
+#     Given a string of text, a type of tokenizer to use, and a type of syllabifier to apply;
+#     Return the text in the format of the tokenized string each breaks into a sequence of syllables.
+#     '''
+#     # normalized navajo data
+#     if language.lower() in ['navajo', 'nvjob']:
+#         normalize_navajo(text)
+#
+#     # segment punctuations from words
+#     segmented = basic_tokenizer(text, tokenizer_type)
+#
+#     # break words into syllables
+#     if syllabifier_type:
+#         segmented = syllabifier(segmented, syllabifier_type)
+#
+#
+#
+#     return segmented
